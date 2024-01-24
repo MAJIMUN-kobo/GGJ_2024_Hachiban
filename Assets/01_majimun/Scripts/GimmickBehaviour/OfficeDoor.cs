00e0104650f793785d5e8189ef647a00be345432
majimun <yanbarumajimun@ryukyugame.page>
2024-01-24 22:58:00 +0900

    「ドア」ギミックの追加

--- Assets/01_majimun/Scripts/GimmickBehaviour/OfficeDoor.cs
+++ Assets/01_majimun/Scripts/GimmickBehaviour/OfficeDoor.cs
@@ -6,11 +6,16 @@ using UnityEngine.Playables;
 public class OfficeDoor : GimmickBehaviour
 {
     [SerializeField] private PlayableDirector _timeline;
+    [SerializeField] private AudioClip        _doorSE;
+    [SerializeField] private AudioSource      _doorAudio;
 
     // [ Activate gimmick action. ]
     public override void OnTriggerActivation(GameObject target = null)
     {
         _timeline.Play();
+        _doorAudio.PlayOneShot(_doorSE);
+
+        Debug.Log("Office Door!!");
 
         base.OnTriggerActivation(target);
     }
