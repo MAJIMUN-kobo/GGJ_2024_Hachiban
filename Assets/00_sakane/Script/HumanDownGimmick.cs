177a932af4b4b8879c6c3426cd8e991911590d04
sakanekazuki0825 <124118351+sakanekazuki0825@users.noreply.github.com>
2024-01-27 22:53:05 +0900

    走って行ってぶつかる人

--- Assets/00_sakane/Script/HumanDownGimmick.cs
+++ Assets/00_sakane/Script/HumanDownGimmick.cs
@@ -1,10 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HumanDownGimmick : GimmickBehaviour
 {
+	// アニメーション
 	Animator animator;
+	// 物理
+	Rigidbody rb;
+	// true = 走っている
+	bool isRunning = false;
+	// 速度
+	[SerializeField]
+	float speed;
 
+	private void Awake()
+	{
+		// アニメーション取得
+		animator = GetComponent<Animator>();
+		// 物理取得
+		rb = GetComponent<Rigidbody>();
+	}
 
+	private void OnCollisionEnter(Collision collision)
+	{
+		// フロア、プレイヤー、以外で、走っている状態だったら衝突
+		if (!collision.gameObject.CompareTag("Floor") &&
+			!collision.gameObject.CompareTag("Player") &&
+			isRunning)
+		{
+			// アニメーション再生
+			animator.SetTrigger("Down");
+			// 動きを止める
+			rb.velocity = Vector3.zero;
+			// 重力無効
+			rb.useGravity = false;
+			// 物理無効
+			rb.isKinematic = true;
+			// コライダー無効
+			GetComponent<Collider>().enabled = false;
+		}
+	}
+
+	/// <summary>
+	/// 走る
+	/// </summary>
+	void Run()
+	{
+		// 走るアニメーション
+		animator.SetTrigger("Run");
+		// 移動開始
+		rb.velocity = transform.forward * speed;
+	}
 }
